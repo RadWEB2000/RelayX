65944cb5973b62cd3615819cb3286c4ec4448299
RadWEB2000 <radoslaw.adamczyk2000@gmail.com>
2025-09-22 23:56:36 +0200

    first

---
+++ src/components/views/home/index.ts
@@ -0,0 +1,3 @@
+export { default as Hero } from "@/views/home/Hero.astro"
+export { default as Overview } from "@/views/home/Overview.astro"
+export { default as Services } from "@/views/home/Services.astro"
